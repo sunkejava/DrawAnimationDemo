5709a6412ba3ed952f3d71cc78a9e3d514c0fe47
sunkejava_WorkPc <1577972070@qq.com>
2019-01-30 17:50:34 +0800

    添加项目文件。

---
+++ DrawAnimationDemo/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace DrawAnimationDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.customHxjdtControl1 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl2 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl3 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl4 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl5 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl6 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl7 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl8 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl9 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl10 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.customHxjdtControl11 = new DrawAnimationDemo.CustomControl.CustomHxjdtControl();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(595, 328);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.customHxjdtControl11);
+            this.tabPage1.Controls.Add(this.customHxjdtControl10);
+            this.tabPage1.Controls.Add(this.customHxjdtControl9);
+            this.tabPage1.Controls.Add(this.customHxjdtControl8);
+            this.tabPage1.Controls.Add(this.customHxjdtControl7);
+            this.tabPage1.Controls.Add(this.customHxjdtControl6);
+            this.tabPage1.Controls.Add(this.customHxjdtControl5);
+            this.tabPage1.Controls.Add(this.customHxjdtControl4);
+            this.tabPage1.Controls.Add(this.customHxjdtControl3);
+            this.tabPage1.Controls.Add(this.customHxjdtControl2);
+            this.tabPage1.Controls.Add(this.customHxjdtControl1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(587, 302);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "环形进度条";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(587, 302);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "柱形进度条";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // customHxjdtControl1
+            // 
+            this.customHxjdtControl1.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl1.CircularWidth = 16;
+            this.customHxjdtControl1.Lcp = System.Drawing.Drawing2D.LineCap.Flat;
+            this.customHxjdtControl1.Location = new System.Drawing.Point(8, 6);
+            this.customHxjdtControl1.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl1.Name = "customHxjdtControl1";
+            this.customHxjdtControl1.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl1.TabIndex = 0;
+            this.customHxjdtControl1.Text = "customHxjdtControl1";
+            this.customHxjdtControl1.Value = 30;
+            // 
+            // customHxjdtControl2
+            // 
+            this.customHxjdtControl2.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl2.CircularWidth = 16;
+            this.customHxjdtControl2.Lcp = System.Drawing.Drawing2D.LineCap.Square;
+            this.customHxjdtControl2.Location = new System.Drawing.Point(104, 6);
+            this.customHxjdtControl2.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl2.Name = "customHxjdtControl2";
+            this.customHxjdtControl2.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl2.TabIndex = 1;
+            this.customHxjdtControl2.Text = "customHxjdtControl2";
+            this.customHxjdtControl2.Value = 30;
+            // 
+            // customHxjdtControl3
+            // 
+            this.customHxjdtControl3.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl3.CircularWidth = 16;
+            this.customHxjdtControl3.Lcp = System.Drawing.Drawing2D.LineCap.Round;
+            this.customHxjdtControl3.Location = new System.Drawing.Point(199, 6);
+            this.customHxjdtControl3.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl3.Name = "customHxjdtControl3";
+            this.customHxjdtControl3.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl3.TabIndex = 2;
+            this.customHxjdtControl3.Text = "customHxjdtControl3";
+            this.customHxjdtControl3.Value = 30;
+            // 
+            // customHxjdtControl4
+            // 
+            this.customHxjdtControl4.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl4.CircularWidth = 16;
+            this.customHxjdtControl4.Lcp = System.Drawing.Drawing2D.LineCap.Triangle;
+            this.customHxjdtControl4.Location = new System.Drawing.Point(294, 6);
+            this.customHxjdtControl4.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl4.Name = "customHxjdtControl4";
+            this.customHxjdtControl4.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl4.TabIndex = 3;
+            this.customHxjdtControl4.Text = "customHxjdtControl4";
+            this.customHxjdtControl4.Value = 30;
+            // 
+            // customHxjdtControl5
+            // 
+            this.customHxjdtControl5.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl5.CircularWidth = 16;
+            this.customHxjdtControl5.Lcp = System.Drawing.Drawing2D.LineCap.NoAnchor;
+            this.customHxjdtControl5.Location = new System.Drawing.Point(389, 6);
+            this.customHxjdtControl5.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl5.Name = "customHxjdtControl5";
+            this.customHxjdtControl5.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl5.TabIndex = 4;
+            this.customHxjdtControl5.Text = "customHxjdtControl5";
+            this.customHxjdtControl5.Value = 30;
+            // 
+            // customHxjdtControl6
+            // 
+            this.customHxjdtControl6.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl6.CircularWidth = 16;
+            this.customHxjdtControl6.Lcp = System.Drawing.Drawing2D.LineCap.SquareAnchor;
+            this.customHxjdtControl6.Location = new System.Drawing.Point(485, 6);
+            this.customHxjdtControl6.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl6.Name = "customHxjdtControl6";
+            this.customHxjdtControl6.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl6.TabIndex = 5;
+            this.customHxjdtControl6.Text = "customHxjdtControl6";
+            this.customHxjdtControl6.Value = 30;
+            // 
+            // customHxjdtControl7
+            // 
+            this.customHxjdtControl7.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl7.CircularWidth = 16;
+            this.customHxjdtControl7.Lcp = System.Drawing.Drawing2D.LineCap.RoundAnchor;
+            this.customHxjdtControl7.Location = new System.Drawing.Point(8, 102);
+            this.customHxjdtControl7.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl7.Name = "customHxjdtControl7";
+            this.customHxjdtControl7.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl7.TabIndex = 6;
+            this.customHxjdtControl7.Text = "customHxjdtControl7";
+            this.customHxjdtControl7.Value = 30;
+            // 
+            // customHxjdtControl8
+            // 
+            this.customHxjdtControl8.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl8.CircularWidth = 16;
+            this.customHxjdtControl8.Lcp = System.Drawing.Drawing2D.LineCap.DiamondAnchor;
+            this.customHxjdtControl8.Location = new System.Drawing.Point(104, 102);
+            this.customHxjdtControl8.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl8.Name = "customHxjdtControl8";
+            this.customHxjdtControl8.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl8.TabIndex = 7;
+            this.customHxjdtControl8.Text = "customHxjdtControl8";
+            this.customHxjdtControl8.Value = 30;
+            // 
+            // customHxjdtControl9
+            // 
+            this.customHxjdtControl9.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl9.CircularWidth = 16;
+            this.customHxjdtControl9.Lcp = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            this.customHxjdtControl9.Location = new System.Drawing.Point(390, 102);
+            this.customHxjdtControl9.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl9.Name = "customHxjdtControl9";
+            this.customHxjdtControl9.Size = new System.Drawing.Size(185, 185);
+            this.customHxjdtControl9.TabIndex = 8;
+            this.customHxjdtControl9.Text = "customHxjdtControl9";
+            this.customHxjdtControl9.Value = 30;
+            // 
+            // customHxjdtControl10
+            // 
+            this.customHxjdtControl10.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl10.CircularWidth = 16;
+            this.customHxjdtControl10.Lcp = System.Drawing.Drawing2D.LineCap.AnchorMask;
+            this.customHxjdtControl10.Location = new System.Drawing.Point(200, 102);
+            this.customHxjdtControl10.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl10.Name = "customHxjdtControl10";
+            this.customHxjdtControl10.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl10.TabIndex = 9;
+            this.customHxjdtControl10.Text = "customHxjdtControl10";
+            this.customHxjdtControl10.Value = 30;
+            // 
+            // customHxjdtControl11
+            // 
+            this.customHxjdtControl11.BackColor = System.Drawing.Color.DarkGray;
+            this.customHxjdtControl11.CircularWidth = 16;
+            this.customHxjdtControl11.Lcp = System.Drawing.Drawing2D.LineCap.Custom;
+            this.customHxjdtControl11.Location = new System.Drawing.Point(294, 102);
+            this.customHxjdtControl11.MainColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(92)))), ((int)(((byte)(138)))));
+            this.customHxjdtControl11.Name = "customHxjdtControl11";
+            this.customHxjdtControl11.Size = new System.Drawing.Size(90, 90);
+            this.customHxjdtControl11.TabIndex = 10;
+            this.customHxjdtControl11.Text = "customHxjdtControl11";
+            this.customHxjdtControl11.Value = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 328);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private CustomControl.CustomHxjdtControl customHxjdtControl1;
+        private CustomControl.CustomHxjdtControl customHxjdtControl3;
+        private CustomControl.CustomHxjdtControl customHxjdtControl2;
+        private CustomControl.CustomHxjdtControl customHxjdtControl8;
+        private CustomControl.CustomHxjdtControl customHxjdtControl7;
+        private CustomControl.CustomHxjdtControl customHxjdtControl6;
+        private CustomControl.CustomHxjdtControl customHxjdtControl5;
+        private CustomControl.CustomHxjdtControl customHxjdtControl4;
+        private CustomControl.CustomHxjdtControl customHxjdtControl9;
+        private CustomControl.CustomHxjdtControl customHxjdtControl10;
+        private CustomControl.CustomHxjdtControl customHxjdtControl11;
+    }
+}
+
